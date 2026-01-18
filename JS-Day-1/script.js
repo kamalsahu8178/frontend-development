324d98bdd9cd8ef5c580ded0c25565676408b87c
Kamal Sahu <kamalsahu8178@gmail.com>
2026-01-18 21:07:46 +0530

    Add files via upload

---
+++ JS-Day-1/script.js
@@ -0,0 +1,37 @@
+
+// // REGULAR FUNCTION
+//        function sum(){
+//             let a=10;
+//             let b=20;
+//             console.log(a+b);
+//         }
+//         sum();
+
+// // ARROW FUNCTION
+//         const sum=(a,b)=>{
+//             console.log(a+b);
+//         }
+//         sum(5,10);
+
+
+// // callback function
+// function sample(callback){
+//     console.log("inside sample function");
+//     callback();
+// }
+// function demo(){
+//     console.log("inside demo function");
+// }
+// sample(demo);
+
+// // call back function
+// function sample(callback){
+//     console.log("sample");
+//     callback()
+// }
+// function demo(){
+//     console.log("demo");
+// }
+// sample(damo)
+
+
---
+++ JS-Day-2/style.js
@@ -0,0 +1,40 @@
+// let arr=[1,2,3,4,5,6]
+// let x = arr.map((y)=>y)
+// console.log(x)
+
+
+
+
+// let arr=[10,15,20,40,32,21,12,11,40]
+// let x = arr.filter((y)=>{
+//     if(arr>=15){
+//         console.log("pass")
+//     }
+//     else{
+//         console.log("fail")
+//     }
+// });
+
+// let scores = [12,22,14,15,32,20,6,17];
+// let highscore =
+// scores.filter((score) =>{
+//     if(score>=15){
+//         console.log(score,"pass")
+//     }
+//     else{
+//         console.log(score,"fail")
+//     }
+// });
+
+// let x=[1,2,3,4,]
+// for(let i=0;i<x;i++){
+//     sum += x[i]
+//     console.log(sum)
+// }
+
+// let x=[1,2,3,4,]
+// sum=0
+// for(let i=0;i<x;i++){
+//     sum = sum + x[i]
+//     console.log(sum)
+// }
