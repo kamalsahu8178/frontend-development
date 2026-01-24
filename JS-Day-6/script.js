9137063fbb8bfb82d6fef1c9443ab58b25fa791a
Kamal Sahu <kamalsahu8178@gmail.com>
2026-01-24 13:25:55 +0530

    Add files via upload

---
+++ JS-Day-6/script.js
@@ -0,0 +1,40 @@
+// let sentence = "The quick brown fox jumps over the lazy dog";
+// console.log(sentence.length);
+// let words = sentence.split(" ");
+// console.log(words);
+
+// //Count the occurance of 'r' in a given string using split method
+// let str='tu meri me tera me tera tu meri'
+// console.log(str.split('r').length - 1);
+
+// //Return: it will return only once 
+// function test(){
+//     console.log("test")
+//     return function test2(){
+//         console.log("test2")
+//         return function test3(){
+//             console.log("test3")
+//         }
+//     }
+
+// }
+// // test()()()   // function currying:
+// let value=test()
+// let value2=value()
+// value2()
+
+
+////Anonymous function:
+// Map(function() {
+//     console.log("Anonymous function")
+// })
+
+//IIFE: Immediately invoked function expression
+// (function(){
+//     let n=3
+//     let a=10**n
+//      b=a
+//     console.log(a)
+// })
+// ()
+// console.log(b)
