168661f65d8a0ffe002b754218f08957b8e2ba32
Kamal Sahu <kamalsahu8178@gmail.com>
2026-01-24 13:25:04 +0530

    Add files via upload

---
+++ JS-Day-4/script.js
@@ -0,0 +1,15 @@
+// let  number =  [1,2,3,4,5]
+// let slice = number.slice(1,4);
+// console.log(slice);
+
+// let number = [1,2,3,4,5]
+// let even = number.some(x => x % 2 === 0 );
+// console.log(even);
+
+// let number = [2,2,2,2,2]
+// let even = number.every(x => x % 2 === 0 );
+// console.log(even);
+
+// let number = [1,2,3,4]
+// number.splice(0,2,5,6);
+// console.log(number)
