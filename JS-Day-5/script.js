e02f6c9b5ea28b203e6a30d8c42ed080b2561964
Kamal Sahu <kamalsahu8178@gmail.com>
2026-01-24 13:25:33 +0530

    Add files via upload

---
+++ JS-Day-5/script.js
@@ -0,0 +1,30 @@
+// let str = "welcome to java script"
+// let sliced = str.slice(0,10);   negative number do not use in slice (-1,2) but ude in substring  
+// console.log(sliced)
+
+// let str = "welcome to java script"
+// let sliced = str.substring(5,15);
+// console.log(sliced)
+
+// string literals
+// let Name= "ma0nta"
+// let lastname="nnt"
+// let fame=`${Name} last namee is ${lastname}`;
+// console.log(fame)
+
+// let university="K.R mangalam university"
+// let program="BCA(Ai&DS)"
+// let Namer="kamal sahu"
+// console.log("MY NAME IS "+Namer+" I AM FROM "+ university + " I AM CURRENTLY PURSUING " + program)
+
+
+// SPREAD operator
+let arr=[2,8,4,5,3,6]
+let see=[5,7,4,7,9,]
+let newarr=[...arr,...see];
+console.log(newarr)
+
+// // dob sir ka dakh
+// // 24-9-1998
+// 30 min bache h BaseAudioContext
+// hno 41 naybaste ,odissa
